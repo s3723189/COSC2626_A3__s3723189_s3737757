49912e2b8c351b94e71ee0cbe8c006c05621abcd
s3723189 <s3723189@student.rmit.edu.au>
2021-06-03 08:59:19 +1000

    added login feature

--- main.py
+++ main.py
@@ -1,11 +1,63 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
-
+import requests
+import json
 app = Flask(__name__)
-
+app.secret_key = 'SECRET_KEY'
 @app.route('/')
 def root():
 
     return render_template("index.html")
+
+
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+
+    if request.method == 'POST':
+        email_para = request.form['email']
+        password_para = request.form['password']
+        request_query = "https://5aegj1tt7i.execute-api.us-east-1.amazonaws.com/"\
+        "default/helloworld?email={email}&password={password}".format(email = email_para, password = password_para)
+        result = requests.get(url = request_query)
+        data = result.json()
+        if len(data) == 0:
+            return render_template('login.html')
+        
+        data = data[0]
+        log_in_user(data[1], data[2], data[3], data[0])
+        return render_template('index.html')
+
+
+    else:
+        return render_template('login.html')
+
+
+
+
+    return "testing"
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template('index.html')
+
+
+
+def log_in_user(name, password, email, primary_key):
+    session['username'] = name
+    session['password'] = password
+    session['email'] = email
+    session['primary_key'] = primary_key
+
+    
+
+
+
+def is_logged_in():
+    exists = False
+    if "username" in session:
+        exists = True
+    return exists
 
 
 if __name__ == '__main__':
