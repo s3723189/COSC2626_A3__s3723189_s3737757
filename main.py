ea2b17289c7f8108ff29a8cd0b9fbf79c6b33bd1
Giorady <s3737757@student.rmit.edu.au>
2021-06-08 03:26:08 +1000

    adjusted the structure to fit the new one

--- main.py
+++ main.py
@@ -1,40 +1,17 @@
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 import requests
 import json
+
+from routes import *
 app = Flask(__name__)
+app.register_blueprint(routes)
+
 app.secret_key = 'SECRET_KEY'
 @app.route('/')
 def root():
 
     return render_template("index.html")
 
-
-
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-
-    if request.method == 'POST':
-        email_para = request.form['email']
-        password_para = request.form['password']
-        request_query = "https://5aegj1tt7i.execute-api.us-east-1.amazonaws.com/"\
-        "default/helloworld?email={email}&password={password}".format(email = email_para, password = password_para)
-        result = requests.get(url = request_query)
-        data = result.json()
-        if len(data) == 0:
-            return render_template('login.html')
-        
-        data = data[0]
-        log_in_user(data[1], data[2], data[3], data[0])
-        return render_template('index.html')
-
-
-    else:
-        return render_template('login.html')
-
-
-
-
-    return "testing"
 
 @app.route('/logout')
 def logout():
@@ -50,8 +27,6 @@ def log_in_user(name, password, email, primary_key):
     session['primary_key'] = primary_key
 
     
-
-
 
 def is_logged_in():
     exists = False
