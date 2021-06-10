b95ebf54e2a02da61cd5960224796510c03d57a1
s3723189 <s3723189@student.rmit.edu.au>
2021-06-11 01:28:30 +1000

    completed various features

--- routes/__init__.py
+++ routes/__init__.py
@@ -3,12 +3,9 @@ from flask import Blueprint
 routes = Blueprint('routes', __name__)
 
 from .login import *
-<<<<<<< HEAD
 from .explore import *
 from .trending import *
 from .movie import *
-=======
 from .register import *
-from .explore import *
+from .watchlist import *
 
->>>>>>> 2b5a85fa050ade945f739ad4aa60a2854520a095
---
+++ routes/watchlist.py
@@ -0,0 +1,24 @@
+from flask import render_template
+from flask import Flask, render_template, redirect, url_for, request, session, flash, Blueprint
+from . import routes
+
+import requests
+@routes.route('/watchlist/<int:movie_name>')
+def watchlist(movie_name):
+
+    print(movie_name)
+
+    wl_query = "https://5aegj1tt7i.execute-api.us-east-1.amazonaws.com/default"\
+    "/helloworld?action=wladd&id={id}&mov={mov}"\
+    "".format(id = session["primary_key"], mov = movie_name)
+
+    result = requests.get(url = wl_query)
+    
+    data = result.json()
+
+    
+
+    
+    return redirect(url_for('root'))
+
+    
