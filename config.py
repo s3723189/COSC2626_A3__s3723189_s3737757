800639162cef9562f4bf1233f09ed7d94e2b286d
Giorady <s3737757@student.rmit.edu.au>
2021-06-12 23:11:08 +1000

    Updated S3 url to Cloudfront url

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 S3_BUCKET                 = "XXX"
 S3_KEY                    = "XXX"
 S3_SECRET                 = "XXX"
-S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
+#S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
+S3_LOCATION                = "https://d1jphzkpp95em7.cloudfront.net/"
 
 SECRET_KEY                = os.urandom(32)
 DEBUG                     = True
