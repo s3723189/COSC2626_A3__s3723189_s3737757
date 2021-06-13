bbbac856343655dbad53752e35250601d38daf08
Gabriel Lat (s3723189) <61673758+s3723189@users.noreply.github.com>
2021-06-13 22:46:53 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-S3_BUCKET                 = "s3737757-s3723189-a3"
-S3_KEY                    = "AKIA6QWQLLEFA7HQ3S5O"
-S3_SECRET                 = "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"
+S3_BUCKET                 = "XXX"
+S3_KEY                    = "XXX"
+S3_SECRET                 = "XXX"
 #S3_LOCATION               = 'http://{}.s3.amazonaws.com/'.format(S3_BUCKET)
 S3_LOCATION                = "https://d1jphzkpp95em7.cloudfront.net/"
 
