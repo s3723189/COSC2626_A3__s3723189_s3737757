fca2f9e394f6198fd57f85045b020ba8b75f171b
s3723189 <s3723189@student.rmit.edu.au>
2021-06-13 17:10:48 +1000

    fixed issua with root

--- application.py
+++ application.py
@@ -40,6 +40,5 @@ if __name__ == '__main__':
     # the "static" directory. See:
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
-    application.run(host='127.0.0.1', port=8080, debug=True)
-    
-
+    application.debug = True
+    application.run()
